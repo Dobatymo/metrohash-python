8b686a6f68545d19765bd79452842403cce4af38
Aurélien Lambert <aure@olli-ai.com>
2020-10-19 13:50:00 +0700

    match `hashlib` behavior: - enable `update` after `digest` - add `copy` method - add `digest_size`, `block_size` and `name` readonly attributes
    Signed-off-by: Aurélien Lambert <aure@olli-ai.com>
    
    ...

--- metrohash.pyx
+++ metrohash.pyx
@@ -3,6 +3,7 @@
 import sys
 from libcpp cimport bool
 from libc.stdint cimport uint64_t, uint8_t
+from cython.operator cimport dereference as deref
 
 """
 h = MetroHash128()
@@ -25,6 +26,7 @@ cdef extern from "metrohash.h" nogil:
 
 	cdef cppclass CMetroHash64 "MetroHash64":
 		CMetroHash64(const uint64_t seed)
+		CMetroHash64(const CMetroHash64& copy)
 		void Initialize(const uint64_t seed)
 		void Update(const uint8_t* buffer, const uint64_t length)
 		void Finalize(uint8_t* const result)
@@ -33,6 +35,7 @@ cdef extern from "metrohash.h" nogil:
 
 	cdef cppclass CMetroHash128 "MetroHash128":
 		CMetroHash128(const uint64_t seed)
+		CMetroHash128(const CMetroHash128& copy)
 		void Initialize(const uint64_t seed)
 		void Update(const uint8_t* buffer, const uint64_t length)
 		void Finalize(uint8_t* const result)
@@ -63,13 +66,28 @@ cpdef bytes metrohash128(bytes data, uint64_t seed=0ULL):
 cdef class MetroHash64(object):
 
 	cdef CMetroHash64* _hasher
-	cdef bytearray _hash
-	cdef bool _notfinal
 
-	def __cinit__(self, uint64_t seed=0ULL):
-		self._hasher = new CMetroHash64(seed)
-		self._hash = bytearray(b"\0"*8)
-		self._notfinal = True
+	@property
+	def digest_size(self):
+		return 8
+
+	@property
+	def block_size(self):
+		return 32
+
+	@property
+	def name(self):
+		return 'metrohash64'
+
+	def __cinit__(self, seed=0):
+		cdef uint64_t _seed
+		cdef MetroHash64 _copy
+		if isinstance(seed, MetroHash64):
+			_copy = <MetroHash64> seed
+			self._hasher = new CMetroHash64(deref(_copy._hasher))
+		else:
+			_seed = <uint64_t> seed
+			self._hasher = new CMetroHash64(_seed)
 
 		if self._hasher is NULL:
 			raise MemoryError()
@@ -80,34 +98,44 @@ cdef class MetroHash64(object):
 			self._hasher = NULL
 
 	def update(self, data):
-		if self._notfinal:
-			self._hasher.Update(data, len(data))
-		else:
-			raise RuntimeError("Hash already finalized.")
+		self._hasher.Update(data, len(data))
 
-	def digest(self):
-		if self._notfinal:
-			self._hasher.Finalize(self._hash)
-			self._notfinal = False
-
-		return bytes(self._hash)
+	cpdef digest(self):
+		cdef bytearray _hash = bytearray(8)
+		CMetroHash64(deref(self._hasher)).Finalize(_hash)
+		return bytes(_hash)
 
 	def hexdigest(self):
 		return bytes2hex(self.digest())
+
+	def copy(self):
+		return MetroHash64(self)
 
 cdef class MetroHash128(object):
 
 	cdef CMetroHash128* _hasher
-	cdef bytearray _hash
-	cdef bool _notfinal
 
-	def __cinit__(self, uint64_t seed=0ULL):
-		self._hasher = new CMetroHash128(seed)
-		self._hash = bytearray(b"\0"*16)
-		self._notfinal = True
+	@property
+	def digest_size(self):
+		return 16
 
-		if self._hasher is NULL:
-			raise MemoryError()
+	@property
+	def block_size(self):
+		return 64
+
+	@property
+	def name(self):
+		return 'metrohash128'
+
+	def __cinit__(self, seed=0):
+		cdef uint64_t _seed
+		cdef MetroHash128 _copy
+		if isinstance(seed, MetroHash128):
+			_copy = <MetroHash128> seed
+			self._hasher = new CMetroHash128(deref(_copy._hasher))
+		else:
+			_seed = <uint64_t> seed
+			self._hasher = new CMetroHash128(_seed)
 
 	def __dealloc__(self):
 		if self._hasher is not NULL:
@@ -115,17 +143,15 @@ cdef class MetroHash128(object):
 			self._hasher = NULL
 
 	def update(self, data):
-		if self._notfinal:
-			self._hasher.Update(data, len(data))
-		else:
-			raise RuntimeError("Hash already finalized.")
+		self._hasher.Update(data, len(data))
 
-	def digest(self):
-		if self._notfinal:
-			self._hasher.Finalize(self._hash)
-			self._notfinal = False
-
-		return bytes(self._hash)
+	cpdef digest(self):
+		cdef bytearray _hash = bytearray(16)
+		CMetroHash128(deref(self._hasher)).Finalize(_hash)
+		return bytes(_hash)
 
 	def hexdigest(self):
 		return bytes2hex(self.digest())
+
+	def copy(self):
+		return MetroHash128(self)
